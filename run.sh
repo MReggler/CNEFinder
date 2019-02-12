f24eabd689606fedc2e0515565ddedab31fa10ea
Matt Reggler <m.reggler@warwick.ac.uk>
2019-02-12 12:37:54 +0000

    Update docker-compose implementation
    Set and update entrypoint script and refactor how compose handles args as env vars.

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
-#! /bin/sh
+#!/bin/sh
 
-./cnef -r $REF_GENOME_FILE -q $QUERY_GENOME_FILE \
-    -e $EXONS_REF_FILE -f $EXONS_QUERY_FILE \
-    -y $REF_CHROM -z $QUERY_CHROM \
-    -a $REF_START -b $REF_END -c $QUERY_START -d $QUERY_END \
-    -t $SIM_THRESHOLD -l $MIN_SEQ_LENGTH \
-    -o $OUTPUT_FILE
+chmod +x ./cnef
+
+./cnef -r ${REF_GENOME_FILE} -q ${QUERY_GENOME_FILE} \
+    -e ${EXONS_REF_FILE} -f ${EXONS_QUERY_FILE} \
+    -y ${REF_CHROM} -z ${QUERY_CHROM} \
+    -a ${REF_START} -b ${REF_END} -c ${QUERY_START} -d ${QUERY_END} \
+    -t ${SIM_THRESHOLD} -l ${MIN_SEQ_LENGTH} \
+    -o ${OUTPUT_PATH}
