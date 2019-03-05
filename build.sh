b0f9846d029ccacda3f603f6bca4b87fda7a2299
Matt Reggler <m.reggler@warwick.ac.uk>
2019-03-05 20:42:49 +0000

    Add 'parse_bed' docker build to build script

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "Building cnefinder image..."
-docker build --rm=true --file docker/Dockerfile -t cnefinder .
+docker build --rm=true --file docker/cnefinder/Dockerfile -t cnefinder .
 echo "Built cnefinder image."
 
+
+echo "Building parse_bed image..."
+docker build --rm=true --file docker/parse_bed/Dockerfile -t parse_bed .
+echo "Built parse_bed image."
 
 # Add more docker build commands below
